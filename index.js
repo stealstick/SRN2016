7468cded6b7f6deecd13accecb018146926cf39d
m4stem4 <m4stem4@naver.com>
2016-09-15 02:18:54 +0900

    design photopage and text box

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 function heightResize() {
-	var photoPage = document.getElementById('photo');
 	var mainPage = document.getElementById('main');
 	var photo = document.getElementById('test');
 	mainPage.style.height = window.innerHeight + 'px';
 	mainPage.style.width = window.innerWidth + 'px';
-	photoPage.style.height = window.innerHeight + 700 + 'px';
 }
 window.onload = function() {
 	heightResize();
