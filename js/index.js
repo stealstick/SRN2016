e99604ede77cf6ff73597326388a3fb01df48154
stealstick <stillstick40@gmail.com>
2016-09-23 11:00:03 +0900

    pc view edit

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ function heightResize() {
 	var mainPage = document.getElementById('main');
 	var photo = document.getElementById('test');
 	mainPage.style.height = window.innerHeight + 'px';
-	mainPage.style.width = window.innerWidth + 'px';
+	mainPage.style.width = (window.innerWidth+30) + 'px';
 }
 
 
